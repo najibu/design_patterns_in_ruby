c346339a8676e25c5402f6102ff344b2f462d821
najibu <ns.najibu@gmail.com>
2016-12-14 11:57:54 +0000

    Added adapter

---
+++ adapter/runtime_modifications/british_text_object.rb
@@ -0,0 +1,3 @@
+class BritishTextObject < TextObject
+  attr_reader :string, :size_mm, :color
+end
--- example.rb
+++ example.rb
@@ -1,12 +1,92 @@
-class FileDeleteCommand
-  def initialize(path)
-    @path = path
+require 'rubygems'
+require 'madeleine'
+
+class Employee
+  attr_accessor :name, :number, :address
+
+  def initialize(name, number, address)
+    @name = name
+    @number = number
+    @address = address
   end
 
-  def execute
-    File.delete(@path)
+  def to_s
+    "Employee: name #{name} num: #{number} addr: #{address}"
   end
 end
 
-fdc = FileDeleteCommand.new('foo.dat')
-fdc.execute
+class EmployeeManger < Employee
+  def initialize
+    @employees = {}
+  end
+
+  def add_employee(e)
+    @employees[e.number] = e
+  end
+
+  def change_address(number, address)
+    employee = @employees[number]
+    raise "No such employee" if not employee
+    employee.address = address
+  end
+
+  def find_employee
+    @employees[number]
+  end
+end
+
+class AddEmployee
+  def initialize(employee)
+    @employee = employee
+  end
+
+  def execute(system)
+    system.add_employee(@employee)
+  end
+end
+
+class DeleteEmployee
+  def initialize(number)
+    @number = number
+  end
+
+  def execute(system)
+    system.delete(@number)
+  end
+end
+
+class ChangeAddress
+  def initialize(number, address)
+    @number = number
+    @address = address
+  end
+
+  def execute(system)
+    system.change_address(@number, @address)
+  end
+end
+
+class FindEmployee
+  def initialize(number)
+    @number = number
+  end
+
+  def execute(system)
+    system.change_address(@number)
+  end
+end
+
+store = SnapshotMadeleine.new('employees') [EmployeeManger.new]
+
+Thread.new do 
+  while true
+    sleep(20)
+    madeleine.take_snapshot
+  end
+end
+
+tom = Employee.new('tom', '1001', '1 Division Street')
+harry = Employee.new('harry', '1002', '3435 Sunnyside Ave')
+
+store.execute_command(AddEmployee.new(tom))
+store.execute_command(AddEmployee.new(harry))
