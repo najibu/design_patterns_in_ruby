7bb444091c5877ff50efdc13476ee95223b75f71
najibu <ns.najibu@gmail.com>
2016-12-16 11:36:07 +0000

    Added singleton

--- example.rb
+++ example.rb
@@ -1,18 +1,22 @@
-class TestMethodMissing
-  def hello
-    puts "Hello from a real method"
+class ClassVariableTester
+  @@class_count = 0 
+  def initialize
+    @instance_count = 0
   end
 
-  def method_missing(name, *args)
-    puts "Warning, warning, unknown method called: #{name}"
-    puts "Arguments: #{args.join(' ')}"
+  def increment
+    @@class_count += 1 
+    @instance_count += 1
+  end
+
+  def to_s
+    "class_count: #{@@class_count} instance_count: #{@instance_count}"
   end
 end
 
-tmm = TestMethodMissing.new 
-tmm.hello
-
-tmm.goodbye('cruel', 'world')
-
-tmm.send(:hello)
-tmm.send(:goodbye, 'cruel', 'world')
+c1 = ClassVariableTester.new
+c1.increment
+c1.increment
+puts "c1: #{c1}"
+c2 = ClassVariableTester.new
+puts "c2: #{c2}"
---
+++ singleton/class_as_singleton/class_based_logger.rb
@@ -0,0 +1,33 @@
+class ClassBasedLogger
+  ERROR = 1 
+  WARNING = 2
+  INFO = 3 
+
+  @@level = WARNING 
+
+  def self.log 
+    @@log ||= File.open('log.txt', 'w')
+  end
+
+  def self.error(msg)
+    log.puts("Error: #{msg}")
+    log.flush 
+  end
+
+  def self.warning(msg)
+    log.puts("Warning: #{msg}") if @@level >= WARNING
+  end
+
+  def self.info(msg)
+    log.puts "Info:#{msg}" if @@level >= INFO
+    log.flush
+  end
+
+  def self.level=(new_level)
+    @@level = new_level
+  end
+
+  def self.level
+    @@level
+  end
+end
